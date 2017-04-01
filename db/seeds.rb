d8041a07bf52f760ee9aefb27f7e13a53a8bdc27
dwoods447 <dwoods447@gmail.com>
2017-04-01 01:08:41 -0500

    amped up seeds.rb - frontEnd changes - minified js files 4-1-2017

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,7 +83,7 @@ federal_benefit_info = [
     [991606, "CH 1606 - Montgomery GI Selected Reserve (MGIB-SR)"],
     [991607, "CH 1607 - Reserve Educational Assistance(REAP)"],
     [999931, "CH 31 - Vocational Rehabilitation and Employement "] ,
-    [999935, "CH 35 - Dependents Educational Assistance"],
+    [999931, "CH 35 - Dependents Educational Assistance"],
     [999201, "Military Tuition Assistance"],
     [990001, "I dont have any"]
 
@@ -108,23 +108,65 @@ students_info = [
     [7106965, 1114, "Margeret", " Rodriguez", "832-415-8554", "MargeretRRodriguez@lonestar.edu", 999935, 999983],
     [7108745, 1115,  "Danica ", "Salcido", "713-548-1159", "DanicaJSalcido@lonestar.edu", 999931, 990002 ],
     [7104172, 1116, "Britt", " Goss", "832-415-4225", "BrittTGoss@lonestar.edu", 991606, 999981],
-    [7106514, 1112, "Katherine ", " Leonard", "713-511-1515", "KatherineALeonard@lonestar.edu",990001, 999982 ],
+    [7106514, 1112, "Katherine ", " Leonard", "713-511-1515", "KatherineALeonard@lonestar.edu",990001, 999982],
     [7107784, 1114, "Michael ", " Marx",  "818-215-4552", "MichaelKMarx@lonestar.edu", 990001, 990002],
     [7195584, 1114, "Kyle ", " Defro",  "713-215-4533", "KyleDefro2@lonestar.edu", 999933, 990002],
-
-
-
-
-
     [7054974 , 1114, "Zachery", "Mathis", "850-377-4501" , "non.egestas.a@est.edu", 990001, 999984],
     [7392249, 1113,  "Bryar", "Collier", "902-201-1551", "dolor@Phasellus.com", 999201, 990002],
-    [7420549, 1111, "Regan", "Flowers", "832-915-1486", "vestibulum@ornare.net", 999930, 999981 ],
+    [7420549, 1111, "Regan", "Flowers", "832-915-1486", "vestibulum@ornare.net", 999930, 999981],
     [7719888, 1114, "Hedda", "Romero", "832-415-8554", "iaculis@Cumsociisnatoque.edu", 999935, 999983],
-    [7445524, 1115,  "Rosalyn", "Conway", "713-548-1159", "sodales.nisi.magna@arcuVivamus.org", 999931, 990002 ],
+    [7445524, 1115,  "Rosalyn", "Conway", "713-548-1159", "sodales.nisi.magna@arcuVivamus.org", 999931, 990002],
     [7799957, 1116, "Larissa", "Shields", "832-415-4225", "feugiat@nisisem.net", 991606, 999981],
-    [7317062, 1112, "Declan", "Dixon", "713-511-1515", "elementum@libero.edu",990001, 999982 ],
-    [7822789, 1114, "Mohammad", "Mckay",  "818-215-4552", "gravida@mi.co.uk", 990001, 990002],
-    [7338896, 1114, "Samson", "Barlow",  "713-215-4533", "condimentum@semperegestas.com", 999933, 990002]
+    [7317062, 1112, "Declan", "Dixon", "713-511-1515", "elementum@libero.edu",990001, 999982],
+    [7822789, 1114, "Mohammad", "Mckay",  "818-215-4552", "gravida@mi.edu", 990001, 990002],
+    [7338896, 1114, "Sam", "Barlow",  "713-215-4533", "condimentum@semperegestas.com", 999933, 990002],
+    [7846627,	 1113, 	"Walker",	"Leon",	"242-482-7933", 	"velit.eget.laoreet@enimsitamet.edu", 	 999931,	 999982],
+    [7268319,	 1113, 	"Sloane",	"Goodman",	"219-413-3485",	"sagittis.semper@nonarcuVivamus.org",	 991606 ,	 999984],
+    [7989014,	 1116,	"Keiko",	"Vance"	"433-740-2349",	"purus.Maecenas.libero@dictum.com",	 999931,	 999983],
+    [7144853,	 1112, 	"Wilma",	"Bullock",	"948-495-1723",	"Sed@dolorquam.net",	99993, 	 990002],
+    [7087356,	1111, 	"Ian"	, "Bridges",	"743-334-8536",	"Pellentesque.habitant@dignissimpharetraNam.edu",	 999201, 	 999982],
+
+
+
+
+
+    [7047358,	1112,	"Stuart",	"Bond",	"107-150-2295",	"sem.Pellentesque@pharetra.org",	999201,	999984],
+    [7135206,	1112,	"Yen",	"Bartlett",	"346-322-2357",	"Suspendisse.sagittis@vehiculaaliquet.com",	991606,	999984],
+    [7290148,	1116,	"Emi",	"Cleveland",	"290-646-9118",	"tincidunt.vehicula.risus@vitaevelitegestas.edu",	999930,	999984],
+    [7518961,	1112,	"Orlando",	"Mclaughlin",	"943-767-2051",	"molestie.pharetra@arcuVestibulumante.edu",	999930,	990002 ],
+    [7837595,	1113,	"Keely",	"Valenzuela",	"773-614-1651",	"adipiscing.ligula@CraspellentesqueSed.edu", 	999201,	999982 ],
+    [7088295,	1112,	"Hunter",	"West",	"253-969-6172",	"Ut.semper@CuraePhasellus.org",	999933,	999981],
+
+
+
+
+
+
+
+
+    [7460394, 1112,	"Rana",	"George",	"830-830-0811",	"Nullam@miac.org", 999931, 	999984],
+    [7895011, 1113, "Pearl", 	"Hardy", 	"376-161-5472",	"sollicitudin.adipiscing@aliquet.net",	991606,	999981],
+    [7725940,	1112,	"Destiny",	"Vaughn",	"622-389-2026",	"cursus.purus.Nullam@anteblandit.edu",	999931,	999982],
+    [7491420,	1115,	"Eric",	"Stanton",	"206-836-3985",	"nec.leo@lectus.ca",	999933,	999983],
+    [7968163,	1111,	"Garrett",	"Rosario",	"288-808-2858",	"In.tincidunt.congue@aliquameuaccumsan.org",	991607,	999982],
+    [7108407,	1111,	"Colette", "Blackburn",	"891-952-2734",	"magna.Ut.tincidunt@tincidunt.ca",	990001,	999984],
+    [7285769,	1111,	"Colton",	"Boyd",	"297-454-4079",	"Integer@Inscelerisque.ca",	990001,	999982],
+    [7356553,	1111,	"Nora",	"Peck",	"182-265-7395",	"lorem.sit@felis.com",	999931,	999981],
+    [7444747,	1113,	"Cecilia",	"Mcfarland","397-638-7170",	"aptent.taciti@loremloremluctus.edu",	999931,	999983],
+    [7969361,	1112, "Hilda",	"Mcneil",	"980-832-7967",	"ultrices.Vivamus@ipsum.edu",	991607,	999981],
+    [7869184,	1116,	"Wallace",	"Trevino",	"659-592-0070",	"velit.Sed.malesuada@gravidaAliquam.net",	991606,	999981],
+
+
+
+    [7529072,	1114,	"Autumn",	"Curry",	"769-628-8599",	"est.ac@dolor.edu",	999201,	990002],
+    [7499009,	1111,	"Sigourney",	"Maxwell",	"794-122-4143",	"in@nislelementumpurus.ca",	991607,	999982],
+    [7860889,	1115,	"Rhoda",	"Gutierrez",	"727-505-4311",	"fermentum@natoquepenatibus.net",	991607,	999983],
+    [7432091,	1115,	"Emery",	"Wallace",	"468-110-9706",	"enim@pulvinararcuet.com",	999931,	990002],
+    [7954978,	1112,	"Rooney",	"Workman",	"486-115-4440",	"ipsum.ac.mi@lectusconvallisest.ca",	999933,	999983],
+    [7681843,	1112,	"Kibo",	"Whitfield",	"719-894-2860",	"tempus.lorem.fringilla@sodales.ca",	999930,	990002]
+
+
+
 
 ]
 
@@ -144,22 +186,60 @@ certifiers_info =[
 
 student_visits_info =[
 
- [7101357, 7101357, true, "I need to change my degree plan", "", "", 1],
- [7195584, 7101357, true, "I added GEOL 1303 ",  "", "", 2],
- [7108745, 7001151, true, "I dropped Math 1310",  "", "", 3],
- [7106965, 7146151, true, "I my BAH stiped was not the full amount",  "", "", 4],
- [7108745, 7001121, true, "I'm a new transfer student and i dont know what to do first.",  "", "", 5],
+    [7101357, 7101357, true, "I need to change my degree plan", "", "", 1],
+    [7195584, 7101357, true, "I added GEOL 1303 ",  "", "", 2],
+    [7108745, 7001151, true, "I dropped Math 1310",  "", "", 3],
+    [7106965, 7146151, false, "I my BAH stiped was not the full amount",  "", "", 4],
+    [7108745, 7001121, false, "I'm a new transfer student and i dont know what to do first.",  "", "", 5],
 
 
- [7054974 ,  7101357, false, "I need to change my degree plan", "2016-03-21 04:51:23.062503", "2016-03-21 05:22:49.117839", 1],
- [7392249, 7101357, false, "I need to change my degree plan", "2016-03-22 04:51:23.062503", "2017-03-22 05:22:49.117839",1],
- [7420549, 7229151, false, "I need to change my degree plan", "2016-03-23 04:51:23.062503",   "2017-03-23 05:22:49.117839",1],
- [7719888, 7146151, false, "I dropped Math 1310","2016-10-21 04:51:23.062503", "2017-10-21 05:22:49.117839", 3],
- [7445524, 7146151, false, "I my BAH stiped was not the full amount","2016-10-21 04:59:23.062503", "2017-10-21 08:23:49.117839", 4],
- [7799957, 7101357 , false, "I my BAH stiped was not the full amount","2016-10-22 04:59:23.062503", "2017-10-22 08:23:49.117839", 4],
- [7317062,  7251151 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-22 04:51:23.062503",   "2017-03-21 03:22:49.117839", 5],
- [7822789,  7101357 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-29 04:51:23.062503",   "2017-03-29 08:22:49.117839", 5],
- [7338896, 7101357 , false,  "I'm a new transfer student and i dont know what to do first.","2016-03-20 04:51:23.062503",    "2017-03-20 06:22:49.117839", 5],
+    [7054974 ,  7101357, false, "I need to change my degree plan", "2016-03-21 04:51:23.062503", "2016-03-21 05:22:49.117839", 1],
+    [7392249, 7101357, false, "I need to change my degree plan", "2016-03-22 04:51:23.062503", "2017-03-22 05:22:49.117839",1],
+    [7420549, 7229151, false, "I need to change my degree plan", "2016-03-23 04:51:23.062503",   "2017-03-23 05:22:49.117839",1],
+    [7719888, 7146151, false, "I dropped Math 1310","2016-10-21 04:51:23.062503", "2017-10-21 05:22:49.117839", 3],
+    [7445524, 7146151, false, "I my BAH stiped was not the full amount","2016-10-21 04:59:23.062503", "2017-01-28 08:23:49.117839", 4],
+    [7799957, 7101357 , false, "I my BAH stiped was not the full amount","2016-10-22 04:59:23.062503", "2017-01-10 08:23:49.117839", 4],
+    [7317062,  7251151 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-22 04:51:23.062503",   "2017-03-21 03:22:49.117839", 5],
+    [7822789,  7101357 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-29 04:51:23.062503",   "2017-03-29 08:22:49.117839", 5],
+    [7338896, 7101357 , false,  "I'm a new transfer student and i dont know what to do first.","2016-03-20 04:51:23.062503",    "2017-03-20 06:22:49.117839", 5],
+
+
+    [7954978 ,  7101357, false, "I need to change my degree plan", "2016-03-20 04:51:23.062503", "2016-03-21 05:22:49.117839", 1],
+    [7432091, 7101357, false, "I need to change my degree plan", "2016-03-19 04:51:23.062503", "2017-03-22 05:22:49.117839",1],
+    [7860889, 7229151, false, "I need to change my degree plan", "2016-03-18 04:51:23.062503",   "2017-03-23 05:22:49.117839",1],
+    [7499009, 7146151, false, "I dropped Math 1310","2016-10-19 04:51:23.062503", "2017-10-21 05:22:49.117839", 3],
+    [7529072, 7146151, false, "I my BAH stiped was not the full amount","2016-10-20 04:59:23.062503", "2017-01-16 08:23:49.117839", 4],
+    [7869184, 7101357 , false, "I my BAH stiped was not the full amount","2016-10-19 04:59:23.062503", "2017-01-14 08:23:49.117839", 4],
+    [7969361,  7251151 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-17 04:51:23.062503",   "2017-03-21 03:22:49.117839", 5],
+    [7444747,  7101357 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-16 04:51:23.062503",   "2017-03-29 08:22:49.117839", 5],
+    [7356553, 7101357 , false,  "I'm a new transfer student and i dont know what to do first.","2016-03-18 04:51:23.062503",    "2017-03-20 06:22:49.117839", 5],
+
+
+
+    [7285769 ,  7101357, false, "I need to change my degree plan", "2016-03-13 04:51:23.062503", "2016-03-21 05:22:49.117839", 1],
+    [7108407, 7101357, false, "I need to change my degree plan", "2016-03-16 04:51:23.061141", "2017-03-22 05:22:49.117839",1],
+    [7968163, 7229151, false, "I need to change my degree plan", "2016-03-20 04:51:23.062503",   "2017-03-23 05:22:49.117839",1],
+    [7491420, 7146151, false, "I dropped Math 1310","2016-10-11 04:51:23.062503", "2017-03-18 05:22:49.117839", 3],
+    [7725940, 7146151, false, "I my BAH stiped was not the full amount","2016-10-10 04:59:23.062503", "2017-03-13 08:23:49.117839", 4],
+    [7895011, 7101357 , false, "I my BAH stiped was not the full amount","2016-10-19 04:59:23.062503", "2017-03-12 08:23:49.117839", 4],
+    [7460394,  7251151 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-09 04:51:23.062503",   "2017-03-21 03:22:49.117839", 5],
+    [7088295,  7101357 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-19 04:51:23.062503",   "2017-03-29 08:22:49.117839", 5],
+    [7837595, 7101357 , false,  "I'm a new transfer student and i dont know what to do first.","2016-05-20 04:51:23.062503",    "2017-02-19 06:22:49.117839", 5],
+
+
+
+    [7518961 ,  7101357, false, "I need to change my degree plan", "2016-03-21 04:51:23.062503", "2016-03-21 05:22:49.117839", 1],
+    [7290148, 7101357, false, "I need to change my degree plan", "2016-06-22 04:51:23.062503", "2017-03-22 05:22:49.117839",1],
+    [7135206, 7229151, false, "I need to change my degree plan", "2016-08-23 04:51:23.062503",   "2017-03-23 05:22:49.117839",1],
+    [7047358, 7146151, false, "I dropped Math 1310","2016-10-25 04:51:23.062503", "2017-02-18 05:22:49.117839", 3],
+
+
+    [7846627, 7146151, false, "I my BAH stiped was not the full amount","2016-10-21 04:59:23.062503", "2017-01-21 08:23:49.117839", 4],
+    [7268319, 7101357 , false, "I my BAH stiped was not the full amount","2016-10-22 04:59:23.062503", "2017-01-22 08:23:49.117839", 4],
+    [7989014,  7251151 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-22 04:52:23.062503",   "2017-03-21 03:22:49.117839", 5],
+    [7144853,  7101357 , false,  "I'm a new transfer student and i dont know what to do first.", "2016-03-29 04:53:23.062503",   "2017-03-29 08:22:49.117839", 5],
+    [7087356, 7101357 , false,  "I'm a new transfer student and i dont know what to do first.","2016-03-20 04:55:23.062503",    "2017-03-20 06:22:49.117839", 5],
+
 
 
 ]
@@ -189,7 +269,7 @@ reason_for_visit.each do |reason_for_visit_id, visit_reason_name|
   reason << ReasonForVisit.create(reason_for_visit_id: reason_for_visit_id, visit_reason_name: visit_reason_name)
 end
 user_info.each do |first_name, last_name, email_addr, username, password |
- user << User.create(first_name: first_name, last_name: last_name, email: email_addr, username: username, password: password)
+  user << User.create(first_name: first_name, last_name: last_name, email: email_addr, username: username, password: password)
 end
 
 
@@ -205,7 +285,7 @@ end
 
 
 state_benefit_info.each do |state_benefit_id, benefit_name|
-    state_benefit << StateBenefit.create(state_benefit_id: state_benefit_id, benefit_name: benefit_name)
+  state_benefit << StateBenefit.create(state_benefit_id: state_benefit_id, benefit_name: benefit_name)
 end
 
 
